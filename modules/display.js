6e9d7ed01409596e6776c0b66534fab601c4df3d
danielsafari143 <danielsafari143@gmail.com>
2023-05-01 16:54:35 +0200

    ES6 AWESOME-BOOKS

---
+++ modules/display.js
@@ -0,0 +1,14 @@
+// import removeItem from "./remove.js";
+const displayBooks = (booksList) => {
+  const booksContainer = document.getElementById('books');
+  booksContainer.innerHTML = '';
+  for (let i = 0; i < booksList.length; i += 1) {
+    booksContainer.innerHTML += `
+            <br>
+            <div class="main-div">  <p class="pargraph-books">"${booksList[i].title}" by ${booksList[i].author}</p>
+            <div class="buton-books"> <button  id="remove${i}" onclick="removeItem(${i})" >Remove</button><br/></div> 
+            <br/></div>
+          `;
+  }
+};
+export default displayBooks;
